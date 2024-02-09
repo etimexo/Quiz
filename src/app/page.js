e7bca458c825b32682fdcd14032fdec3bf6bc95f
Elijah Obisesan <elijahobisesan01@gmail.com>
2024-02-09 22:42:15 +0100

    Style opening page

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="w-full bg-blue-800 h-[100vh] flex justify-center align-center">
       <Link href= "./quiz">
-        <div>Start Quiz</div>
+        <div className="flex justify-center align-center text-white bg-slate-800 text-2xl rounded-md p-12 mt-[40%] font-black">Start Quiz</div>
       </Link>
     </div>
   );

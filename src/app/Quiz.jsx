0bf59dea993d78663aba0a61a237579d7392f9e2
Elijah Obisesan <elijahobisesan01@gmail.com>
2024-02-09 17:09:21 +0100

    Complete logic

--- src/app/Quiz.jsx
+++ src/app/Quiz.jsx
@@ -1,56 +1,102 @@
-"use client"
+"use client";
 import React, { useState } from "react";
-import { quiz } from "./quizData.js"
+import { quiz } from "./quizData.js";
 // import Question from "./Question";
 
 export default function Quiz() {
   // const { activeQ, setActiveQ } = useState(0);
   // const { index, setIndex } = useState(0);
-  const [activeQuestion, setActiveQuestion] = useState(0)
-  const [selectedAnswer, setSelectedAnswer] = useState('')
-  const [checked, setChecked] = useState(false)
-  const [showResult, setShowResult] = useState(null)
-  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null)
+  const [activeQuestion, setActiveQuestion] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+  const [checked, setChecked] = useState(false);
+  const [showResult, setShowResult] = useState(null);
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
   const [result, setResult] = useState({
     score: 0,
     correctAnswers: 0,
     wrongAnswers: 0,
-  })
-  const {questions} = quiz;
-  const {question, answers, correctAnswer} = questions[activeQuestion]
+  });
+  const { questions } = quiz;
+  const { question, answers, correctAnswer } = questions[activeQuestion];
+  const onAnswerSelected = (answer, idx) => {
+    setChecked(true)
+    setSelectedAnswerIndex(idx)
+    if (answer === correctAnswer) {
+      setSelectedAnswer(true)
+      console.log(('true'))
+    } else {
+      setSelectedAnswer(false)
+      console.log('false')
+    }
+  }
+
+  const nextQuestion = () => {
+    setSelectedAnswerIndex(null)
+    setResult((prev) =>
+      selectedAnswer ?
+      {
+        ...prev,
+        score: prev.score + 5,
+        correctAnswers: prev.correctAnswers + 1
+      } : {
+        ...prev,
+        wrongAnswers: prev.wrongAnswers + 1,
+      }
+    )
+    if (activeQuestion !== questions.length -1) {
+      setActiveQuestion((prev) => prev + 1)
+    } else {
+      setActiveQuestion(0)
+      setShowResult(true)
+    }
+    setChecked(false)
+  }
 
   return (
-    <div className="container">
-    <div>
-      <h1>Quiz</h1>
-      <h2>Question {activeQuestion + 1}<span>/{questions.length}</span></h2>
-      {/* <Question /> */}
-    </div>
-    <div>
-      {!showResult ? (
-      <div className="quiz-container">
-        <h3>{questions[activeQuestion].question}</h3>
-        {answers.map((answer, idx) => (
-          <li 
-            key={idx}
-            className={selectedAnswerIndex === idx ? 'li-selected' : 'li-hover'}
-          >
-            <span>{answer}</span>
-          </li>
-        //   <li
-        //    key={idx}
-        //    className={
-        //     selectedAnswerIndex === idx ? 'li-selected : 'li-hover'
-        // }
-        // >
-        //     <span>{answer}</span>
-        //   </li>
-        ))}
+    <div className="container flex-row p-48">
+      <div>
+        <h1>Quiz</h1>
+        <h2>
+          Question {activeQuestion + 1}
+          <span>/{questions.length}</span>
+        </h2>
+        {/* <Question /> */}
       </div>
-      ) : (
-        <div className="quiz-container"></div>
-      )}
-    </div>
+      <div>
+        {!showResult ? (
+          <div className="quiz-container">
+            <h3>{questions[activeQuestion].question}</h3>
+            {answers.map((answer, idx) => (
+              <li
+                key={idx}
+                onClick={() => onAnswerSelected(answer, idx)}
+                className={
+                  selectedAnswerIndex === idx ? "li-selected" : "li-hover"
+                }
+              >
+                <span>{answer}</span>
+              </li>
+            ))}
+            {checked ? (
+              <button onClick={nextQuestion}>
+              {activeQuestion === question.length - 1? 'Finish' : 'Next'}
+              </button>
+            ): (
+              <button onClick={nextQuestion} disabled></button>
+            )}
+          </div>
+        ) : (
+          <div className="quiz-container">
+            <h3>Results</h3>
+            <h3>Overall {(result.score / 25) * 100}%</h3>
+            <p>Total Questions: <span>{questions.length}</span></p>
+            {/* <p>Total Score: <span>{result.score}</span></p> */}
+            <p>Correct Answers: <span>{result.correctAnswers}</span></p>
+            <p>Wrong Answers: <span>{result.wrongAnswers}</span></p>
+            <button onClick={() => window.location.reload()}>Restart</button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
